f0c2fb06b597e380c8a82668dc57d4ccf2746db6
Jared Godfrey <godfreyjared@gmail.com>
2026-08-13 19:24:35 -0500

    Finished project one, removed project two, and fine tuned remaining code for clarity.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
 {
-    public class Program
+    public class QuoteGenerator
     {
-        static void Main(string[] args)
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
         {
-            Console.WriteLine("Hello, World");
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
         }
     }
 }
